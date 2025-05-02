57cc3cbb967e0af8f43f016fa77ba28e301feb83
aboodwebpages <aboodff581@gmail.com>
2025-05-02 21:59:38 +0300

    Add files via upload

---
+++ index .js
@@ -0,0 +1,17 @@
+const PI = 3.141;
+
+
+let result1 = 0 ;
+let result2 = 0 ;
+ document.getElementById("sub1").onclick = function(){
+ radius =  document.getElementById("rad1").value;
+result1= PI * 2 * radius ;
+result1=Number(result1);
+result2= PI * radius**2 ;
+result2=Number(result2);
+
+document.getElementById("resul1").textContent = "area is " + result2  +  
+" and the circumference is " + result1 ;
+ }
+
+ 
